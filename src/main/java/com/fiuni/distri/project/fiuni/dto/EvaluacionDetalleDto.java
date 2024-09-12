37d6be4ec5fd9a497138cca7e4d9f22b34984229
Agus3160 <ampharos321@hotmail.com>
2024-09-11 23:26:21 -0400

    Cambios en el Dto de EvaluacionDetalleDto para mantener la integridad con el Domain

--- src/main/java/com/fiuni/distri/project/fiuni/dto/EvaluacionDetalleDto.java
+++ src/main/java/com/fiuni/distri/project/fiuni/dto/EvaluacionDetalleDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class EvaluacionDetalleDto {
     private int evaluacion_id;
-    private int criterio;
+    private String criterio;
     private int puntaje;
     private String comentarios;
 }
